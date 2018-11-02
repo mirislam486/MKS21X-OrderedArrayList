c2851965d1244f742e3d28f1605b6ae79e35f0ff
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2018-11-02 14:26:49 -0400

    Add files via upload

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -3,7 +3,16 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(){
     super();
   }
+
+
   public NoNullArrayList(int StarttingCapacity){
     super(StarttingCapacity);
+  }
+
+
+  public boolean add(E element){
+    E[] arr = new E[arr.length + 1];
+    ArrPlus[arr.length - 1] = element;
+    return true;
   }
 }
