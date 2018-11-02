57f8826dae8e278506f741cf7c4ec4444e426ccc
mirislam486 <34451472+mirislam486@users.noreply.github.com>
2018-11-02 14:19:45 -0400

    Add files via upload

---
+++ NoNullArrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+  public NoNullArrayList(int StarttingCapacity){
+    super(StarttingCapacity);
+  }
+}
